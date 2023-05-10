f5463d7adb4b83028c3be32f8c9c2c5a8599cae0
WilliamWJD <william.jose.wjd@gmail.com>
2023-05-10 08:49:29 -0300

    feat: listando games

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,67 @@
+package com.devsuperior.dslist.dto;
+
+import java.util.Objects;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMinDTO() {
+	}
+
+	public GameMinDTO(Game entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.year = entity.getYear();
+		this.imgUrl = entity.getImgUrl();
+		this.shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	@Override
+	public String toString() {
+		return "GameMinDTO [id=" + id + ", title=" + title + ", year=" + year + ", imgUrl=" + imgUrl
+				+ ", shortDescription=" + shortDescription + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameMinDTO other = (GameMinDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+}
